5e78ed15936f64272c79dedc96e39323d8367c04
Rafael Cardoso <rrafa203@gmail.com>
2021-11-28 11:27:25 -0800

    add html and css

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,72 +1,72 @@
 function generateHTML(data) {
-
-    return `
-    ${JSON.stringify(data)}
-    <!DOCTYPE html>
-    <html lang="en">
-    
-    <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma-rtl.min.css" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="./assets/css/styles.css" />
-    <title>Team Profile Generator</title>
-    </head>
-    
-    <body>
-      <header>
-        <h1 class="title">My Team</h1>
-      </header>
-      <div class="card">
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  
+  <head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma-rtl.min.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="./styles.css" />
+  <title>Team Profile Generator</title>
+  </head>
+  
+  <body>
+    <header class="jumbotron">
+      <h1 class="title has-text-centered">My Team</h1>
+    </header>
+      <div class="tile is-parent is-justify-content-space-evenly  is-flex-wrap-wrap is-flex-direction-row">
         ${renderTeam(data)}
-    </div>
-    
-      <footer class="container text-center py-3">
-        <h3 class="text-dark">&copy;2020 by ${data[0].employeeName}</h3>
-      </footer>
-    </body>
-    </html>
-    `;
+      </div>
+  
+    <footer class="container has-text-centered py-3">
+      <h3 class="text-dark">&copy;2021 by ${data[0].name}</h3>
+    </footer>
+  </body>
+  </html>
+  `;
 }
-
 function renderExtraInfo(element) {
-    console.log('rendering extra info', element);
-    if (element.getRole() === 'manager') {
-        return `<button>Office Number:${(element.officeNumber)}</button>`;
-    }
-
-    if (element.getRole() === 'Engineer') {
-        return `<button>GitHub:${(element.github)}</button>`;
-    }
-
-    if (element.getRole() === 'Intern') {
-        return `<button>School Name:${(element.school)}</button>`;
-    }
+  if (element.getRole() === 'Manager') {
+      return `<p class="subtitle-5">Office No.:${(element.officeNumber)}</p>`;
+  }
+  if (element.getRole() === 'Engineer') {
+      return `<button class="button is-normal">GitHub:${(element.github)}</button>`;
+  }
+  if (element.getRole() === 'Intern') {
+      return `<p class="subtitle-5">School:${(element.school)}</p>`;
+  }
 }
-
+function renderIcon(element) {
+  if (element.getRole() === 'Manager') {
+      return `<span class="material-icons">free_breakfast</span>`;
+  }
+  if (element.getRole() === 'Engineer') {
+      return `<span class="material-icons-outlined">engineering</span>`;
+  }
+  if (element.getRole() === 'Intern') {
+      return `<span class="material-icons">school</span>`;
+  }
+}
 function renderTeam(data) {
-    console.log('rendering team', data);
-    let output = '';
-    data.forEach(element => {
-        console.log('rendering employee', element);
-
-        output += `<div class="card-content">
-        <p class="title">
-          ${(element.name)}
-          ${(element.getRole())}
-        </p>
-        <ol class="subtitle">
-          <li>Employee ID: ${(element.id)}</li>
-          <li>Email Adrress: ${(element.email)}</li>
-          ${renderExtraInfo(element)}
-        </ol>
-      </div>`;
-    })
-    return output;
+  let output = '';
+  data.forEach(element => {
+      output += `<div class="card is-child" id="card-size">
+      <header class="card-header-tile title notification is-success">${(element.name)}
+      <div class="subtitle">${renderIcon(element)}${(element.getRole())}</>
+      </header>
+      <div class="card-content">
+        <p class="subtitle-5">Employee ID: ${(element.id)}</p>
+        <button class="button is-normal">Email Address: ${(element.email)}</button>
+        <div></div>
+        ${renderExtraInfo(element)}
+      </div>
+    </div>`;
+  })
+  return output;
 }
-
 module.exports = generateHTML;
